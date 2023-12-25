c0b848a24423a25a3fbb6d9fee7b8f518dc201e3
Gouiseong <rhdmltjd1324@gmail.com>
2023-12-26 08:44:38 +0900

    WiseSayingController 생성자 추가

--- src/com/ws/wiseSaying/controller/WiseSayingController.java
+++ src/com/ws/wiseSaying/controller/WiseSayingController.java
@@ -1,11 +1,14 @@
 package com.ws.wiseSaying.controller;
-
 import com.ws.Container;
 import com.ws.Rq;
 import com.ws.wiseSaying.service.WiseSayingService;
-
 public class WiseSayingController {
+
 	private WiseSayingService wiseSayingService;
+
+	public WiseSayingController() {
+		wiseSayingService = new WiseSayingService();
+	}
 
 	public void write() {
 		System.out.print("명언 : ");
@@ -13,22 +16,15 @@ public class WiseSayingController {
 		System.out.print("작가 : ");
 		String author = Container.getScanner().nextLine().trim();
 		int id = wiseSayingService.write(content, author);
-
 		System.out.printf("%d번 명언이 등록되었습니다.\n", id);
 	}
-
 	public void list() {
 		wiseSayingService.list();
 	}
-
 	public void remove(Rq rq) {
 		wiseSayingService.remove(rq);
-
 	}
-
 	public void modify(Rq rq) {
-
 		wiseSayingService.modify(rq);
 	}
-
 }
