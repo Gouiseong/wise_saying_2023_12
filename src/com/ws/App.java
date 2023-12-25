f7754182bd00a75d88377fe40aa5d9222297e6b0
Gouiseong <rhdmltjd1324@gmail.com>
2023-12-26 08:35:08 +0900

    명언 수정 기능 구현

--- src/com/ws/App.java
+++ src/com/ws/App.java
@@ -1,27 +1,18 @@
 package com.ws;
-
 import com.ws.system.controller.SystemController;
 import com.ws.wiseSaying.controller.WiseSayingController;
-
 public class App {
-
 	private byte system_status = 1;
-
 	public App() {
-
 	}
-
 	public void run() {
 		System.out.println("== 명언 앱 실행 ==");
-
 		SystemController systemController = new SystemController();
 		WiseSayingController wiseSayingController = new WiseSayingController();
-
 		while (system_status == 1) {
 			System.out.print("명령어 ) ");
 			String cmd = Container.getScanner().nextLine().trim();
 			Rq rq = new Rq(cmd);
-
 			switch (rq.getActionCode()) {
 			case "종료":
 				systemController.exit();
@@ -36,11 +27,13 @@ public class App {
 			case "삭제":
 				wiseSayingController.remove(rq);
 				break;
+			case "수정":
+				wiseSayingController.modify(rq);
+				break;
 			default:
 				System.out.println("존재하지 않는 명령어입니다");
 				break;
 			}
 		}
-
 	}
 }
